7a2d56c3ea930b63aafbc5246a4f1031b2675649
hmohyud <hyder.mohyuddin@gmail.com>
2026-01-06 01:40:28 +0000

    removed baseroot

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/nahjalbalaghah',
   images: {
     remotePatterns: [
       {
